91dc5c7ce1144ff2fb5b2e9829d561804e8931cd
Alberto Baigorria <lbaigorria@outlook.com>
2024-03-29 20:27:17 -0400

    Proyecto Actualizado

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -37,7 +37,7 @@ export class Server {
         });
 
         this.app.listen(this.port, () => {
-            console.log(`Server running on port ${this.port}`);
+            console.log(`Server running on Port ${this.port}`);
         });
 
     }
